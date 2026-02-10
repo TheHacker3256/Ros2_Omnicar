3c692d05bb35155156e1f2337a20b0bbd6d323d0
Noah Whitehouse <ndwhitehouse@outlook.com>
2026-02-10 02:14:25 +0000

    removed build files

--- build/diffdrive_arduino/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nwhit210/omnicar/src/diffdrive_arduino
-# Build directory: /home/nwhit210/omnicar/build/diffdrive_arduino
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
